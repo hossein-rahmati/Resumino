e23ca26f6754f4560033d017fe6685a08429b4dc
parsashky <mohammadrezaparsaii@gmail.com>
2026-06-08 19:28:50 +0330

    modified template and core folder and added folder static in our project by parsashky

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home_view
+from core.views import *
 
 urlpatterns = [
     path("", home_view, name="home"),
+    path('about',about_view,name='about'),
+    path('contact',contact_view,name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     return render(request, "core/home.html")
+def about_view(request):
+    return render(request,'core/about.html')
+def contact_view(request):
+    return render(request,'core/contact.html')
