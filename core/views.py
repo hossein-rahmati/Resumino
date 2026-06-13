653d55e6c07ccf2857009c218094e97531a71f8e
hossein Rahmati <rmthossein@gmail.com>
2026-06-13 21:29:46 +0330

    Fix some crucial project structure files

--- core/views.py
+++ core/views.py
@@ -13,24 +13,23 @@ def home_view(request, author_username=None):
     comments = Comment.objects.filter(status=True)
     if author_username:
         posts: BaseManager[Comment] = posts.filter(author__username=author_username)
-    context = {'comments': comments}
+    context = {"comments": comments}
     return render(request, "core/home.html", context)
 
 
 def about_view(request):
-    return render(request, 'core/about.html')
+    return render(request, "core/about.html")
 
 
 def contact_view(request):
-    return render(request, 'core/contact.html')
+    return render(request, "core/contact.html")
 
 
 API_KEY = os.environ.get("API_KEY")
-client = Groq(api_key="")
+client = Groq(api_key=API_KEY)
 try:
     completion = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
-        messages=[{"role": "user", "content": "Hi"}]
+        model="llama-3.1-8b-instant", messages=[{"role": "user", "content": "Hi"}]
     )
     print("اتصال برقرار است:", completion.choices[0].message.content)
 except Exception as e:
@@ -50,9 +49,9 @@ def ai_resume_coach(request):
                 messages=[
                     {
                         "role": "system",
-                        "content": "تو 'مربی هوشمند رزومینو' هستی. وظیفه تو تحلیل رزومه و مشاوره شغلی به کاربران است. به فارسی پاسخ بده و بسیار صمیمی و حرفه‌ای باش."
+                        "content": "تو 'مربی هوشمند رزومینو' هستی. وظیفه تو تحلیل رزومه و مشاوره شغلی به کاربران است. به فارسی پاسخ بده و بسیار صمیمی و حرفه‌ای باش.",
                     },
-                    {"role": "user", "content": user_message}
+                    {"role": "user", "content": user_message},
                 ],
                 temperature=0.7,
                 max_tokens=1024,
@@ -63,6 +62,9 @@ def ai_resume_coach(request):
 
         except Exception as e:
             print(f"Groq Error: {e}")  # برای دیدن خطا در ترمینال
-            return JsonResponse({"reply": "اوپس! ارتباطم با مغز متفکرم قطع شده. دوباره امتحان کن."}, status=500)
+            return JsonResponse(
+                {"reply": "اوپس! ارتباطم با مغز متفکرم قطع شده. دوباره امتحان کن."},
+                status=500,
+            )
 
     return JsonResponse({"error": "Invalid request"}, status=400)
